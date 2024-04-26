da7d5c70e9da2e34af0554e111f91ac8dc80c9c0
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2024-04-26 08:47:12 +0200

    Update LevelBoundary.cs

--- Assets/Scripts/Environment/LevelBoundary.cs
+++ Assets/Scripts/Environment/LevelBoundary.cs
@@ -9,7 +9,6 @@ public class LevelBoundary : MonoBehaviour
     public float internalLeft;
     public float internalRight;
 
-    // Update is called once per frame
     void Update()
     {
         internalLeft = leftSide;
