93c4d1d79cdb0b47c21c74665d4319a9c29c97cd
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2024-05-04 21:13:14 +0200

    defined initial random rotation position for the coins

--- Assets/Scripts/Collectables/RotateObject.cs
+++ Assets/Scripts/Collectables/RotateObject.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class RotateObject : MonoBehaviour
 {
     public float rotateSpeed = 1;
+    private float startingPos = 0;
 
+    private void Start()
+    {
+        startingPos = Random.Range(0, 360); // defines initial rotation position
+        transform.Rotate(0, 0, startingPos, Space.Self);
+    }
     void Update()
     {
         transform.Rotate( 0,0, rotateSpeed, Space.Self);
