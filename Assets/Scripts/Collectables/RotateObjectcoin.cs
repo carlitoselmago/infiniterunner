930b1158acc5e27122d196b5deef9627eee3d0c7
carlitoselmago <ccarbonell@gmail.com>
2024-05-13 11:18:21 +0200

    fixed coins texture

---
+++ Assets/Scripts/Collectables/RotateObjectcoin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjectcoin : MonoBehaviour
+{
+    public float rotateSpeed = 1;
+    private float startingPos = 0;
+
+    private void Start()
+    {
+        startingPos = Random.Range(0, 360); // defines initial rotation position
+        //transform.Rotate(0, 0, startingPos, Space.Self);
+       // startingPos = transform.position.z*5f;
+        transform.Rotate(0,startingPos, 0, Space.Self);
+    }
+    void Update()
+    {
+        transform.Rotate( 0,rotateSpeed, 0, Space.Self);
+    }
+}
