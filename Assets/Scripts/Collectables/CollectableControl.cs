b67f37223092b72286b1932de951a45a539fa3f7
carlitoselmago <ccarbonell@gmail.com>
2024-05-15 22:31:36 +0200

    added basic achievement progressive behaviour for coins

--- Assets/Scripts/Collectables/CollectableControl.cs
+++ Assets/Scripts/Collectables/CollectableControl.cs
@@ -9,14 +9,60 @@ public class CollectableControl : MonoBehaviour
     public GameObject coinCountDisplay;
     public GameObject coinEndDisplay;
 
-    void Start(){
+    //achievements vars
+    public GameObject achievementUI;
+    public GameObject achievementEndUItext;
+    public static List<int> treballadordelmes_coins = new List<int> { 5, 50 };
+    private int treballadordelmes_coins_index = 0;
+    //private static int maxTimePlayed;
+    //private float maxTimeResting = 60;
+
+    // private bool HighScoreAchieved = false;
+    // private bool SurvivalAchieved = false;
+    //private bool RestfulAchieved = false;
+
+
+    void Start()
+    {
         coinCount = 0;
-         coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
+        coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
         coinEndDisplay.GetComponent<Text>().text = "" + coinCount;
+        achievementUI.SetActive(false);
     }
     void Update()
     {
         coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
         coinEndDisplay.GetComponent<Text>().text = "" + coinCount;
+
+        if (treballadordelmes_coins_index < treballadordelmes_coins.Count)
+        {
+            if (coinCount >= treballadordelmes_coins[treballadordelmes_coins_index])
+            {
+                //highScoreSFX.Play();
+                achievementEndUItext.GetComponent<Text>().text = "¡treballador del mes! \n" + treballadordelmes_coins[treballadordelmes_coins_index].ToString() + " monedes!";
+                achievementUI.SetActive(true);
+                treballadordelmes_coins_index += 1;
+                StartCoroutine(hideachievement());
+            }
+        }
+        /*
+         if (coinCount >= maxScore && !HighScoreAchieved)
+        {
+            highScoreSFX.Play();
+            highScorePopUp.SetActive(true);
+            HighScoreAchieved = true;
+            //StartCoroutine FadeOutFrame;
+
+            //record new high score!
+        }
+        */
+    }
+
+    IEnumerator hideachievement()
+    {
+        yield return new WaitForSeconds(6);
+        achievementUI.SetActive(false);
     }
 }
+
+
