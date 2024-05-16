be4b4ba317327e0a8447c28c1d6794eb95d360aa
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2024-05-16 09:21:18 +0200

    added force shield flickering

--- Assets/Scripts/Environment/flickering.cs
+++ Assets/Scripts/Environment/flickering.cs
@@ -9,8 +9,16 @@ public class flickering : MonoBehaviour
     public float minWaitTime = 0.1f;
     public float maxWaitTime = 0.5f;
 
+    public bool energyShield = false;
+    public Renderer shield;
+    public bool isFlickering = true;
+
     private void Start()
     {
+        if (energyShield)
+        {
+            shield = GetComponent<Renderer>();
+        }
         StartCoroutine(Flicker());
     }
 
@@ -21,14 +29,32 @@ public class flickering : MonoBehaviour
             // Wait for random time
             yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
 
-            // Set random intensity
-            pointLight.intensity = Random.Range(minIntensity, maxIntensity);
+            if (isFlickering)
+            {
+                // Set random intensity
+                pointLight.intensity = Random.Range(minIntensity, maxIntensity);
 
-            // Wait for a short time
-            yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+                if (energyShield)
+                {
+                    // Toggle the renderer
+                    shield.enabled = !shield.enabled;
+                }
 
-            // Reset intensity to its original value
-            pointLight.intensity = maxIntensity;
+                // Wait for a short time
+                yield return new WaitForSeconds(Random.Range(0.05f, 0.1f));
+
+                // Reset intensity to its original value
+                pointLight.intensity = maxIntensity;
+            }
+        }
+    }
+
+    public void ToggleFlickering()
+    {
+        isFlickering = !isFlickering;
+        if (!isFlickering && shield)
+        {
+            shield.enabled = true; //Ensure renderer is enabled when flickering is off
         }
     }
 }
