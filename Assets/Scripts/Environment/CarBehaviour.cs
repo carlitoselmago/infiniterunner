4759d71f9fd78bd7830906e1a2bf903d4d491a9e
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2024-05-21 09:58:56 +0200

    fixed car behaviour

--- Assets/Scripts/Environment/CarBehaviour.cs
+++ Assets/Scripts/Environment/CarBehaviour.cs
@@ -1,47 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class CarBehaviour : MonoBehaviour
 {
-    private Animator animator;
-    public string animationTriggerName = "carmoving";  // Name of the trigger to activate the animation
-
-    private float timer;
     private float interval;
+    public Animator animator;
 
     void Start()
     {
-        animator = GetComponent<Animator>();
-        if (animator == null)
-        {
-            Debug.LogError("Animator component not found on this GameObject.");
-            enabled = false;  // Disable this script if no Animator component is found
-            return;
-        }
-        SetRandomInterval();
+        animator.enabled = false;
+        interval = Random.Range(0f, 8f);
+        StartCoroutine(Activate());
     }
 
-    void Update()
+    IEnumerator Activate()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= interval)
-        {
-            ActivateAnimator();
-            SetRandomInterval();
-            timer = 0f;
-        }
-    }
-
-    void SetRandomInterval()
-    {
-        interval = Random.Range(0f, 16f);
-    }
-
-    void ActivateAnimator()
-    {
-        if (animator != null)
-        {
-            animator.SetTrigger(animationTriggerName);
-        }
+        yield return new WaitForSeconds(interval);
+        animator.enabled = true;
     }
 }
