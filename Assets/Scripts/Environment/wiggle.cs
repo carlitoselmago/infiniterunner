b2d05b990ec80b4afaeeb62195c2a42174425d45
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-06-12 17:22:13 +0200

    commented wiggle

--- Assets/Scripts/Environment/wiggle.cs
+++ Assets/Scripts/Environment/wiggle.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 
 public class FloatingAndRotating : MonoBehaviour
 {
@@ -25,3 +25,4 @@ public class FloatingAndRotating : MonoBehaviour
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
+*/
