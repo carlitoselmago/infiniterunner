c08637b444b55d15401190c0d6477987d2a7b7b2
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-09-04 20:10:15 +0200

    attached set free constrains to rockets

--- Assets/Scripts/Player/Constrain.cs
+++ Assets/Scripts/Player/Constrain.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class Constrain : MonoBehaviour
 {
-    public PlayerMove player;
+    private PlayerMove player;
 
     // Set which positions are constrained in the Inspector
     public bool constrainLeft = false;
     public bool constrainCenter = false;
     public bool constrainRight = false;
 
+    void Start()
+    {
+       GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            player = playerObj.GetComponent<PlayerMove>();
+            if (player == null)
+                Debug.LogError("Constrain: Player object found but no PlayerMove script attached.");
+        }
+        else
+        {
+            Debug.LogError("Constrain: No GameObject with tag 'Player' found in the scene.");
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
+        if (other.gameObject.CompareTag("Player") && player != null)
             player.SetConstrainedPositions(constrainLeft, constrainCenter, constrainRight);
-        }
     }
 }
