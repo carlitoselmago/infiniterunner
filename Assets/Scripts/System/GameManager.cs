af837fcaa22936a5a553c5ca05f2cce25911198a
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-09-11 21:21:03 +0200

    added hide cursor

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public void Start()
+    {
+        Cursor.visible = false;
+    }
+
     public void Restart()
     {
         // Find all active resettables and reset them
         foreach (var resettable in FindObjectsOfType<MonoBehaviour>().OfType<IResettable>())
-        {
             resettable.ResetState();
-        }
 
         // Handle UI separately if needed
         //UIManager.Instance.HideGameOver();
