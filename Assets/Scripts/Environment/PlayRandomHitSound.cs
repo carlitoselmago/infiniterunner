ae1993f1837f8cedd1f1a83cff828c3c208f6286
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-10-06 17:19:33 +0200

    added barrier hit sfxs

---
+++ Assets/Scripts/Environment/PlayRandomHitSound.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayRandomHitSound : MonoBehaviour
+{
+    [Header("Collision Sounds")]
+    public AudioSource audioSource;
+    public AudioClip[] hitClips;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("car"))
+        {
+            if (hitClips.Length == 0 || audioSource == null) return;
+            int index = Random.Range(0, hitClips.Length);
+            audioSource.pitch = Random.Range(0.8f, 1.1f);
+            audioSource.PlayOneShot(hitClips[index]);
+        }
+    }
+}
