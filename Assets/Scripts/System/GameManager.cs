c24de5ef114e90583845fd635ea5861a02a8eef6
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-12-24 13:57:24 +0100

    fixed reset coins

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -6,13 +6,25 @@ public class GameManager : MonoBehaviour
 
     public void Start()
     {
+        ApplyGameStateCursor();
+    }
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+            ApplyGameStateCursor();
+    }
+
+    private void ApplyGameStateCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     public void Restart()
     {
         // Find all active resettables and reset them
-        foreach (var resettable in FindObjectsOfType<MonoBehaviour>().OfType<IResettable>())
+        foreach (var resettable in FindObjectsOfType<MonoBehaviour>(true).OfType<IResettable>())
             resettable.ResetState();
     }
 }
