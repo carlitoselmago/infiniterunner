f350630277d46f6ff916058c1e146bc00f940e8a
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2025-12-30 20:38:12 +0100

    adjusted resettable crates

---
+++ Assets/Scripts/Collectables/ResettableCrate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ResettableCrate : MonoBehaviour, IResettable
+{
+    public void ResetState()
+    {
+        gameObject.SetActive(true);
+
+        foreach (Transform child in transform)
+            child.gameObject.SetActive(true);
+    }
+}
