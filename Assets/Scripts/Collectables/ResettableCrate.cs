b3ecb226f03d435b7120e195c4a9f59a5dde16df
marcvillanuevamir <122529765+marcvillanuevamir@users.noreply.github.com>
2026-01-02 13:19:07 +0100

    checked flying crates working

--- Assets/Scripts/Collectables/ResettableCrate.cs
+++ Assets/Scripts/Collectables/ResettableCrate.cs
@@ -4,8 +4,6 @@ public class ResettableCrate : MonoBehaviour, IResettable
 {
     public void ResetState()
     {
-        //gameObject.SetActive(true);
-
         foreach (Transform child in transform)
             child.gameObject.SetActive(true);
     }
